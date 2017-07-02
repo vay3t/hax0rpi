8ff0987b2f60c4f31e9267e576250e42a15f3b8d
Vay3t <fffyanez@gmail.com>
2017-07-02 18:41:00 -0400

    Update ubuntu-installer.sh

--- ubuntu-installer.sh
+++ ubuntu-installer.sh
@@ -36,6 +36,7 @@ mv overlook-fing-3.0.deb Descargas/
 # install sublime text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
 sudo apt-get install sublime-text
 
 # install bettercap
