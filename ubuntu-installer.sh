9784f00699a1c789d1d6d0b954f43d607f7c976a
Vay3t <fffyanez@gmail.com>
2018-03-21 20:43:32 -0300

    Update ubuntu-installer.sh

--- ubuntu-installer.sh
+++ ubuntu-installer.sh
@@ -7,6 +7,8 @@ sudo apt install -y \
 		gnome-session-flashback \
 		php \
 		libapache2-mod-php \
+		aircrack-ng \
+		mdk3 \
 		adb \
 		fastboot \
 		apache2 \
@@ -46,56 +48,36 @@ sudo apt-get update
 sudo apt-get install sublime-text
 
 # install bettercap
-sudo apt-get install build-essential ruby-dev libpcap-dev -y
-sudo gem install bettercap
+#sudo apt-get install build-essential ruby-dev libpcap-dev -y
+#sudo gem install bettercap
 
 mkdir dedsec
 mkdir arsenal
 cd arsenal
 
-#git clone https://github.com/epinna/weevely3
-#cd weevely3
-#sudo apt-get install libncurses5-dev
-#sudo pip install -r requirements.txt --upgrade
-#cd ..
-
-# install aircrack-ng
-sudo apt-get -y install libssl-dev libnl-3-dev libnl-genl-3-dev libnl-idiag-3-200 libnl-idiag-3-dev ethtool
-wget http://download.aircrack-ng.org/aircrack-ng-1.2-rc4.tar.gz
-tar -zxvf aircrack-ng-1.2-rc4.tar.gz
-rm aircrack-ng-1.2-rc4.tar.gz
-cd aircrack-ng-1.2-rc4
-make
-sudo make install
-sudo airodump-ng-oui-update
-cd && cd arsenal
+git clone https://github.com/epinna/weevely3
+cd weevely3
+sudo apt-get install libncurses5-dev
+sudo pip install -r requirements.txt --upgrade
+cd ..
 
 # install pixie-dust
-git clone https://github.com/wiire/pixiewps
-cd pixiewps*/
-cd src/
-make
-sudo make install
-cd && cd arsenal
+#git clone https://github.com/wiire/pixiewps
+#cd pixiewps*/
+#cd src/
+#make
+#sudo make install
+#cd && cd arsenal
 
 # install reaver
-sudo apt-get -y install build-essential libpcap-dev sqlite3 libsqlite3-dev
-git clone https://github.com/t6x/reaver-wps-fork-t6x
-cd reaver-wps-fork-t6x*/
-cd src/
-./configure
-make
-sudo make install
-cd && cd arsenal
-
-# instal mdk3
-wget http://aspj.aircrack-ng.org/mdk3-v6.tar.bz2
-tar jxvf mdk3-v6.tar.bz2
-rm mdk3-v6.tar.bz2
-cd mdk3-v6
-make
-sudo make install
-cd && cd arsenal
+#sudo apt-get -y install build-essential libpcap-dev sqlite3 libsqlite3-dev
+#git clone https://github.com/t6x/reaver-wps-fork-t6x
+#cd reaver-wps-fork-t6x*/
+#cd src/
+#./configure
+#make
+#sudo make install
+#cd && cd arsenal
 
 # install sublist3r
 git clone https://github.com/aboul3la/Sublist3r
@@ -114,8 +96,6 @@ sudo update-rc.d bluetooth disable
 sudo update-rc.d dnsmasq disable
 sudo update-rc.d openvpn disable
 sudo update-rc.d ssh disable
-sudo update-rc.d cups disable
-sudo update-rc.d cups-browsed disable
 
 # fix ifaces
 sudo ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
