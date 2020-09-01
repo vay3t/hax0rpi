ef88a9e927b86fdd17c1306d25aa7b27e7e53972
Vay3t <fffyanez@gmail.com>
2020-09-01 15:19:16 -0400

    Update post-snap-install.sh

--- post-snap-install.sh
+++ post-snap-install.sh
@@ -16,21 +16,23 @@ go get -v -u github.com/asciimoo/wuzz
 go get -v -u github.com/tomnomnom/fff
 go get -v -u github.com/tomnomnom/httprobe
 go get -v -u github.com/tomnomnom/waybackurls
-go get -v -u github.com/mvdan/xurls
+go get -v -u github.com/tomnomnom/assetfinder
 go get -v -u github.com/tomnomnom/unfurl
+go get -v -u github.com/tomnomnom/gron
+go get -v -u github.com/mvdan/xurls
 go get -v -u github.com/xxxserxxx/gotop/cmd/gotop
 go get -v -u github.com/BBVA/kapow
-go get -v -u github.com/ffuf/pencode/cmd/pencode
-go get -v -u github.com/tomnomnom/gron
 go get -v -u github.com/davecheney/httpstat
 go get -v -u github.com/OJ/gobuster
 go get -v -u github.com/ffuf/ffuf
+go get -v -u github.com/ffuf/pencode/cmd/pencode
 go get -v -u github.com/ericchiang/pup
 GO111MODULE=on go get -v -u github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+GO111MODULE=on go get -v -u github.com/projectdiscovery/naabu/cmd/naabu
 GO111MODULE=auto go get -v -u github.com/projectdiscovery/httpx/cmd/httpx
 GO111MODULE=auto go get -v -u github.com/projectdiscovery/subfinder/cmd/subfinder
 go get -v -u github.com/haccer/subjack
-go get -v -u github.com/tomnomnom/assetfinder
+
 
 echo 'GOBIN=$HOME/go/bin' >> ~/.bashrc
 echo 'export PATH=$PATH:$GOBIN' >> ~/.bashrc
