863d08ad9afab91e3e88ee530c31bf7a3cfe5847
Vay3t <fffyanez@gmail.com>
2020-09-01 22:30:12 -0400

    Update post-snap-install.sh

--- post-snap-install.sh
+++ post-snap-install.sh
@@ -6,9 +6,10 @@ if [ !$(which snap) ]; then
 fi
 
 # snap
-sudo snap install amass
-sudo snap install go --classic
-
+ &> /dev/null
+if [ !$(which go) ]; then
+    sudo snap install go --classic
+fi
 
 # install golang programs
 go get -v -u github.com/iamstoxe/urlgrab
@@ -19,6 +20,7 @@ go get -v -u github.com/tomnomnom/waybackurls
 go get -v -u github.com/tomnomnom/assetfinder
 go get -v -u github.com/tomnomnom/unfurl
 go get -v -u github.com/tomnomnom/gron
+go get -v -u github.com/tomnomnom/qsreplace
 go get -v -u github.com/mvdan/xurls
 go get -v -u github.com/xxxserxxx/gotop/cmd/gotop
 go get -v -u github.com/BBVA/kapow
@@ -27,11 +29,13 @@ go get -v -u github.com/OJ/gobuster
 go get -v -u github.com/ffuf/ffuf
 go get -v -u github.com/ffuf/pencode/cmd/pencode
 go get -v -u github.com/ericchiang/pup
+go get -v -u github.com/haccer/subjack
+GO111MODULE=on go get -v .u github.com/OWASP/Amass/v3/...
 GO111MODULE=on go get -v -u github.com/projectdiscovery/nuclei/v2/cmd/nuclei
 GO111MODULE=on go get -v -u github.com/projectdiscovery/naabu/cmd/naabu
 GO111MODULE=auto go get -v -u github.com/projectdiscovery/httpx/cmd/httpx
 GO111MODULE=auto go get -v -u github.com/projectdiscovery/subfinder/cmd/subfinder
-go get -v -u github.com/haccer/subjack
+
 
 
 echo 'GOBIN=$HOME/go/bin' >> ~/.bashrc
