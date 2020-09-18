1143a564a60d584033b8f272310d579b2e1b0865
Vay3t <fffyanez@gmail.com>
2020-09-18 00:16:40 -0300

    Update post-snap-install.sh

--- post-snap-install.sh
+++ post-snap-install.sh
@@ -1,48 +1,50 @@
 #!/bin/bash
 
-if [ !$(which snap) ]; then
-    echo "please install snap 'sudo apt install snap'"
-    exit
+source ~/.bashrc
+
+which go &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "please install go 'sudo snap install go --classic'"
+	echo 'GOBIN=$HOME/go/bin' >> ~/.bashrc
+	echo 'export PATH=$PATH:$GOBIN' >> ~/.bashrc
+	exit
 fi
 
-# snap
- &> /dev/null
-if [ !$(which go) ]; then
-    sudo snap install go --classic
-fi
+function exit_trap() {
+	local lc="$BASH_COMMAND" rc=$?
+	echo "Command [$lc] exited with code [$rc]"
+}
 
 # install golang programs
-go get -v -u github.com/iamstoxe/urlgrab
-go get -v -u github.com/asciimoo/wuzz
-go get -v -u github.com/tomnomnom/fff
-go get -v -u github.com/tomnomnom/httprobe
-go get -v -u github.com/tomnomnom/waybackurls
-go get -v -u github.com/tomnomnom/assetfinder
-go get -v -u github.com/tomnomnom/unfurl
-go get -v -u github.com/tomnomnom/gron
-go get -v -u github.com/tomnomnom/qsreplace
-go get -v -u github.com/tomnomnom/gf
-go get -v -u github.com/mvdan/xurls
-go get -v -u github.com/xxxserxxx/gotop/cmd/gotop
-go get -v -u github.com/BBVA/kapow
-go get -v -u github.com/davecheney/httpstat
-go get -v -u github.com/OJ/gobuster
-go get -v -u github.com/ffuf/ffuf
-go get -v -u github.com/ffuf/pencode/cmd/pencode
-go get -v -u github.com/ericchiang/pup
-go get -v -u github.com/haccer/subjack
-go get -v -u github.com/1ndianl33t/urlprobe
-go get -v -u github.com/shenwei356/rush
-GO111MODULE=on go get -v -u github.com/lc/gau
-GO111MODULE=on go get -v .u github.com/OWASP/Amass/v3/...
-GO111MODULE=on go get -v -u github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-GO111MODULE=on go get -v -u github.com/projectdiscovery/naabu/cmd/naabu
-GO111MODULE=on go get -v -u github.com/projectdiscovery/shuffledns/cmd/shuffledns
-GO111MODULE=auto go get -v -u github.com/projectdiscovery/httpx/cmd/httpx
-GO111MODULE=auto go get -v -u github.com/projectdiscovery/subfinder/cmd/subfinder
 
+go get -u github.com/asciimoo/wuzz
+go get -u github.com/tomnomnom/fff
+go get -u github.com/tomnomnom/httprobe
+go get -u github.com/tomnomnom/waybackurls
+go get -u github.com/tomnomnom/assetfinder
+go get -u github.com/tomnomnom/unfurl
+go get -u github.com/tomnomnom/gron
+go get -u github.com/tomnomnom/qsreplace
+go get -u github.com/tomnomnom/gf
+go get -u github.com/mvdan/xurls
+go get -u github.com/xxxserxxx/gotop/cmd/gotop
+go get -u github.com/BBVA/kapow
+go get -u github.com/davecheney/httpstat
+go get -u github.com/OJ/gobuster
+go get -u github.com/ffuf/ffuf
+go get -u github.com/ffuf/pencode/cmd/pencode
+go get -u github.com/ericchiang/pup
+go get -u github.com/haccer/subjack
+go get -u github.com/1ndianl33t/urlprobe
+go get -u github.com/shenwei356/rush
+go get -u github.com/jaeles-project/gospider
+go get -u github.com/hakluke/hakrawler
+GO111MODULE=on go get -u github.com/lc/gau
+GO111MODULE=on go get -u github.com/OWASP/Amass/v3/...
+GO111MODULE=on go get -u github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+GO111MODULE=on go get -u github.com/projectdiscovery/naabu/cmd/naabu
+GO111MODULE=on go get -u github.com/projectdiscovery/shuffledns/cmd/shuffledns
+GO111MODULE=auto go get -u github.com/projectdiscovery/httpx/cmd/httpx
+GO111MODULE=auto go get -u github.com/projectdiscovery/subfinder/cmd/subfinder
 
-
-
-echo 'GOBIN=$HOME/go/bin' >> ~/.bashrc
-echo 'export PATH=$PATH:$GOBIN' >> ~/.bashrc
+echo "[+] Completed"
