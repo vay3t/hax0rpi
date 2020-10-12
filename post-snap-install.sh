552588199e910b9846e4540de0c959ee1c7ca550
Vay3t <fffyanez@gmail.com>
2020-10-12 01:08:34 -0300

    Update post-snap-install.sh

--- post-snap-install.sh
+++ post-snap-install.sh
@@ -36,7 +36,7 @@ go get -u github.com/ffuf/pencode/cmd/pencode &>/dev/null
 go get -u github.com/ericchiang/pup &>/dev/null
 go get -u github.com/haccer/subjack &>/dev/null
 go get -u github.com/1ndianl33t/urlprobe &>/dev/null
-go get -u github.com/jaeles-project/gospider &>/dev/null
+go get -u github.com/hakluke/hakrawler &>/dev/null
 GO111MODULE=on go get -u github.com/lc/gau &>/dev/null
 GO111MODULE=on go get -u github.com/OWASP/Amass/v3/... &>/dev/null
 GO111MODULE=on go get -u github.com/projectdiscovery/nuclei/v2/cmd/nuclei &>/dev/null
