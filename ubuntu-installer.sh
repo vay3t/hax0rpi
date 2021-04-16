d22d0bdd03d1ccc77df55a0305143bfd551161ba
Vay3t <fffyanez@gmail.com>
2021-04-16 18:30:44 -0400

    Update ubuntu-installer.sh

--- ubuntu-installer.sh
+++ ubuntu-installer.sh
@@ -77,8 +77,15 @@ sudo apt-get install -y \
 	xclip \
 	zeal;
 
+# aircrack-ng
 sudo apt install -y aircrack-ng mdk4
 
+# Sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https -y
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text -y
 
 ##### Ruby ######
 
